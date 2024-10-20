9a5b184c024303d91e3b28d144a7fc75f51ab022
VIDHAN GUPTA <148654884+Crusherbolt@users.noreply.github.com>
2024-10-20 18:53:05 +0530

    Delete #!/bin directory

--- #!/bin/network_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Starting server for network test..."
-nc -l -p 12345 # A simple netcat server listening on port 12345
-#This way, your network test client (hwtestlanproprietary.sh) can interact with this server. Upload it on other machine.
-
-
